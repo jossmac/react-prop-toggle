5ccaf6f62099ece07433a9aaeb3523966fd8b5f3
Joss Mackison <joss.mackison@gmail.com>
2018-04-30 12:23:18 +1000

    demo: simplify styles with glam

--- examples/src/index.js
+++ examples/src/index.js
@@ -1,96 +1,18 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
+
 import PropToggle from '../../src';
-
-// styled components
-// ------------------------------
-
-const Container = props => (
-  <div
-    style={{
-      display: 'flex',
-      flexDirection: 'column',
-      height: '100vh',
-      boxSizing: 'border-box',
-      justifyContent: 'center',
-      marginLeft: 'auto',
-      marginRight: 'auto',
-      maxWidth: 400,
-      paddingLeft: 15,
-      paddingRight: 15,
-      textAlign: 'center',
-    }}
-    {...props}
-  />
-);
-const Box = ({ isActive, ...props }) => (
-  <div
-    style={{
-      backgroundColor: 'white',
-      boxShadow: isActive
-        ? '0 1px 5px rgba(0, 0, 0, 0.25), 0 1px 1px rgba(0, 0, 0, 0.1)'
-        : null,
-      borderRadius: 4,
-      lineHeight: 1.4,
-      padding: `30px 30px 40px`,
-    }}
-    {...props}
-  />
-);
-
-const switchSize = 48;
-const Switch = props => (
-  <div
-    style={{
-      position: 'relative',
-      width: switchSize * 2,
-      margin: 'auto',
-      WebkitUserSelect: 'none',
-      msUserSelect: 'none',
-      MozUserSelect: 'none',
-      userSelect: 'none',
-    }}
-    {...props}
-  />
-);
-const SwitchTrack = props => (
-  <div
-    style={{
-      height: switchSize / 2,
-      backgroundColor: 'rgba(0,0,0,0.1)',
-      width: '100%',
-      borderRadius: switchSize / 2,
-    }}
-    {...props}
-  />
-);
-const SwitchHandle = ({ isActive, ...props }) => (
-  <div
-    style={{
-      alignItems: 'center',
-      background: 'linear-gradient(to bottom, white, #fafbfc)',
-      backgroundColor: 'white',
-      borderRadius: '50%',
-      boxShadow: '0 1px 5px rgba(0, 0, 0, 0.25), 0 1px 1px rgba(0, 0, 0, 0.1)',
-      cursor: 'pointer',
-      display: 'flex',
-      filter: `grayscale(${isActive ? '0%' : '100%'})`,
-      fontSize: switchSize / 2,
-      height: switchSize,
-      justifyContent: 'center',
-      left: 0,
-      marginTop: -(switchSize / 2),
-      position: 'absolute',
-      top: '50%',
-      transform: `translateX(${isActive ? '100%' : 0}) rotate(${
-        isActive ? '15deg' : '0deg'
-      })`,
-      transition: 'transform 200ms, filter 200ms',
-      width: switchSize,
-    }}
-    {...props}
-  />
-);
+import {
+  Box,
+  Container,
+  Footer,
+  Header,
+  Repo,
+  Title,
+  Switch,
+  SwitchTrack,
+  SwitchHandle,
+} from './styled';
 
 // example
 // ------------------------------
@@ -114,21 +36,18 @@ class App extends Component {
           attributes={{ class: 'fun', 'data-has-awesome-bg': 'true' }}
         />
         <Box isActive={isActive}>
-          <header>
+          <Header>
             <div>
-              <h1>
+              <Title>
                 {' '}
-                <a
-                  href="https://github.com/jossmac/react-prop-toggle"
-                  style={{ color: 'inherit' }}
-                >
+                <Repo href="https://github.com/jossmac/react-prop-toggle">
                   react-prop-toggle
-                </a>
-              </h1>{' '}
+                </Repo>
+              </Title>{' '}
               is a safe, declarative way to influence the styles and attributes
-              of nodes outside your app's tree.
+              of nodes outside your app's render tree.
             </div>
-          </header>
+          </Header>
           <div>
             <Switch onClick={this.toggle}>
               <SwitchTrack />
@@ -136,12 +55,12 @@ class App extends Component {
             </Switch>
           </div>
         </Box>
-        <footer>
+        <Footer>
           <span> by </span>
           <a href="https://twitter.com/jossmackison" target="_blank">
             @jossmac
           </a>
-        </footer>
+        </Footer>
       </Container>
     );
   }
---
+++ examples/src/styled.js
@@ -0,0 +1,160 @@
+/* @jsx glam */
+import glam from 'glam';
+
+const gutter = 15;
+
+// styled components
+// ------------------------------
+
+export const Container = props => (
+  <div
+    style={{
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+      boxSizing: 'border-box',
+      justifyContent: 'center',
+      marginLeft: 'auto',
+      marginRight: 'auto',
+      maxWidth: 400,
+      paddingLeft: 15,
+      paddingRight: 15,
+      textAlign: 'center',
+    }}
+    {...props}
+  />
+);
+export const Box = ({ isActive, ...props }) => (
+  <div
+    style={{
+      backgroundColor: 'white',
+      boxShadow: isActive
+        ? '0 1px 5px rgba(0, 0, 0, 0.25), 0 1px 1px rgba(0, 0, 0, 0.1)'
+        : null,
+      borderRadius: 4,
+      lineHeight: 1.4,
+      padding: `30px 30px 40px`,
+    }}
+    {...props}
+  />
+);
+
+const switchSize = 48;
+export const Switch = props => (
+  <div
+    style={{
+      position: 'relative',
+      width: switchSize * 2,
+      margin: 'auto',
+      WebkitUserSelect: 'none',
+      msUserSelect: 'none',
+      MozUserSelect: 'none',
+      userSelect: 'none',
+    }}
+    {...props}
+  />
+);
+export const SwitchTrack = props => (
+  <div
+    style={{
+      height: switchSize / 2,
+      backgroundColor: 'rgba(0,0,0,0.1)',
+      width: '100%',
+      borderRadius: switchSize / 2,
+    }}
+    {...props}
+  />
+);
+export const SwitchHandle = ({ isActive, ...props }) => (
+  <div
+    style={{
+      alignItems: 'center',
+      background: 'linear-gradient(to bottom, white, #fafbfc)',
+      backgroundColor: 'white',
+      borderRadius: '50%',
+      boxShadow: '0 1px 5px rgba(0, 0, 0, 0.25), 0 1px 1px rgba(0, 0, 0, 0.1)',
+      cursor: 'pointer',
+      display: 'flex',
+      filter: `grayscale(${isActive ? '0%' : '100%'})`,
+      fontSize: switchSize / 2,
+      height: switchSize,
+      justifyContent: 'center',
+      left: 0,
+      marginTop: -(switchSize / 2),
+      position: 'absolute',
+      top: '50%',
+      transform: `translateX(${isActive ? '100%' : 0}) rotate(${
+        isActive ? '15deg' : '0deg'
+      })`,
+      transition: 'transform 200ms, filter 200ms',
+      width: switchSize,
+    }}
+    {...props}
+  />
+);
+export const Repo = ({ isLocked, ...props }) => (
+  <a
+    target="_blank"
+    css={{
+      borderBottom: '1px solid rgba(0, 0, 0, 0.3)',
+      color: 'inherit',
+      paddingBottom: 1,
+      textDecoration: 'none',
+
+      ':hover': {
+        borderBottomColor: 'rgba(0, 0, 0, 0.6)',
+        textDecoration: 'none',
+      },
+    }}
+    {...props}
+  />
+);
+
+/*
+  ==============================
+  Misc.
+  ==============================
+*/
+
+export const Header = props => (
+  <header css={{ marginBottom: '2em' }} {...props} />
+);
+export const Footer = ({ isVisible, ...props }) => (
+  <footer
+    css={{
+      color: 'white',
+      marginTop: '2em',
+      visibility: isVisible ? 'visible' : 'hidden',
+
+      '& a': { color: 'inherit' },
+    }}
+    {...props}
+  />
+);
+export const Title = props => (
+  <h1
+    css={{
+      display: 'inline',
+      fontSize: 18,
+      fontWeight: 500,
+      letterSpacing: '-0.025em',
+      margin: 0,
+    }}
+    {...props}
+  />
+);
+export const Code = props => (
+  <code
+    css={{
+      backgroundColor: 'rgba(0, 0, 0, 0.09)',
+      borderRadius: '3px',
+      color: '#091e42',
+      display: 'inline-block',
+      fontFamily: '"Monaco", monospace',
+      fontSize: '0.85em',
+      lineHeight: '1.4',
+      padding: '1px 5px',
+    }}
+    {...props}
+  />
+);
